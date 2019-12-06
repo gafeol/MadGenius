b08748390809abbd01856282273844ddb1edabca
robonauta <pedro.hba@hotmail.com>
2019-12-06 10:58:13 +0100

    Splits Gameplay into "GamplayAgility" and "GameplayMemory"; agility mode onclick redirects to the game container now.

--- app/src/main/java/com/example/madgenius/GameplayAgility.java
+++ app/src/main/java/com/example/madgenius/GameplayAgility.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Gameplay extends AppCompatActivity {
+public class GameplayAgility extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gameplay);
+        setContentView(R.layout.activity_gameplay_agility);
     }
 }
--- app/src/main/java/com/example/madgenius/Menu.java
+++ app/src/main/java/com/example/madgenius/Menu.java
@@ -2,7 +2,9 @@ package com.example.madgenius;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void agility_onClick(View view) {
+        Intent intent = new Intent(Menu.this, GameplayAgility.class);
+        startActivity(intent);
     }
 }
