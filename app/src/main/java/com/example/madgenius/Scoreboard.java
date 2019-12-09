a0469a9fc5f11a3fe9bc4f018168d641673b9922
fonticode <fontai@usi.ch>
2019-12-09 02:06:33 +0100

    add activity score chart

--- app/src/main/java/com/example/madgenius/Scoreboard.java
+++ app/src/main/java/com/example/madgenius/Scoreboard.java
@@ -1,16 +1,31 @@
 package com.example.madgenius;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.EditText;
+
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
+
 public class Scoreboard extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.madgenius.username";
+
+
+
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +47,53 @@ public class Scoreboard extends AppCompatActivity {
                 text.setText(R.string.best_agility);
             }
         });
+
         scoreViewModel.getAllScoresOrdered(true).observe(Scoreboard.this, scores -> {
             adapter.setScores(scores);
         });
     }
-}
+
+
+
+
+    public void scoreChartRequest(View view) {
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+            View messageView = getLayoutInflater().inflate(R.layout.dialog_chart_request, null);
+            EditText usernameEditText = messageView.findViewById(R.id.usernameditText);
+
+            Button findChartButton = messageView.findViewById(R.id.ButtonFind);
+            Button cancelButton = messageView.findViewById(R.id.ButtonCancel);
+
+            mBuilder.setView(messageView);
+            AlertDialog dialog = mBuilder.create();
+
+            findChartButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String username = usernameEditText.getText().toString();
+                    if(username.isEmpty()){
+                        Toast.makeText(getApplicationContext(), "Please enter username", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Intent intent = new Intent(getApplicationContext(), ScoreChart.class);
+                        intent.putExtra(EXTRA_MESSAGE, username);
+                        startActivity(intent);
+                        dialog.dismiss();
+                    }
+                }
+
+
+            });
+
+            cancelButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+            dialog.show();
+        }
+    }
+
+
+
