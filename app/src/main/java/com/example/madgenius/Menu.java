90296932bee68c660b51114fb092a193151b648f
gafeol <gabfeol@gmail.com>
2019-12-16 00:15:42 +0100

    Adds login on home menu

--- app/src/main/java/com/example/madgenius/Menu.java
+++ app/src/main/java/com/example/madgenius/Menu.java
@@ -1,28 +1,66 @@
 package com.example.madgenius;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Menu extends AppCompatActivity {
-    private TextView helloTextView;
+    private TextView helloTextView, loginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
         setHelloMessage();
     }
 
     private void setHelloMessage(){
         helloTextView = findViewById(R.id.helloTextView);
+        loginTextView = findViewById(R.id.loginTextView);
         String username = SavedInfo.getUsername(getApplicationContext());
-        if(!username.isEmpty())
-            helloTextView.setText("Hello "+username+"!");
+        if(!username.isEmpty()) {
+            helloTextView.setText("Hello " + username + "!");
+            loginTextView.setText("Not you?");
+        }
+        else{
+            loginTextView.setText("Click here to login");
+        }
+    }
+
+    public void loginMessage(View v){
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        View messageView = getLayoutInflater().inflate(R.layout.dialog_login, null);
+
+        EditText usernameEditText = messageView.findViewById(R.id.usernameEditText);
+        String savedUsername = SavedInfo.getUsername(getApplicationContext());
+        usernameEditText.setText(savedUsername);
+
+        Button saveButton = messageView.findViewById(R.id.saveButton);
+        Button cancelButton = messageView.findViewById(R.id.cancelButton);
+        mBuilder.setView(messageView);
+        AlertDialog dialog = mBuilder.create();
+
+        saveButton.setOnClickListener(view -> {
+            String username = usernameEditText.getText().toString();
+            if(username.isEmpty())
+                usernameEditText.setError("Please fill your username");
+            else {
+                SavedInfo.saveUsername(getApplicationContext(), username);
+                setHelloMessage();
+                dialog.dismiss();
+            }
+        });
+
+        cancelButton.setOnClickListener(view -> {
+            dialog.dismiss();
+        });
+        dialog.show();
     }
 
     @Override
